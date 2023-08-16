d3bb98a4b768645533106c90cfc134f2456095f2
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-08-16 14:30:33 +0200

    clean up , removing hello world app

--- RealPython/rp-portfolio/personal_portfolio/urls.py
+++ RealPython/rp-portfolio/personal_portfolio/urls.py
@@ -17,4 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("hello_world.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
