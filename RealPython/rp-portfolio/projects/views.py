37a45e2f98ae969f03abd3cd3f6b18003f7329b2
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-08-16 15:08:03 +0200

    project list all projects

--- RealPython/rp-portfolio/projects/views.py
+++ RealPython/rp-portfolio/projects/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
+def project_index(request):
+    projects = Project.objects.all()
+    context = {"projects": projects}
+    return render(request, "project_index.html", context)
+
+
+def project_detail(request, pk):
+    """
+    pk: primary key
+    """
+    project = Project.objects.get(pk=pk)
+    context = {"project": project}
+    return render(request, "project_detail.html", context)
